baadc8365aa6d27f24a56cf1fb6b441f634faeec
Raul Barriga <barrigaraulaug@gmail.com>
2020-08-08 13:08:52 -0700

    Workshop 1 new commit
    Here is my finished workshop 1 assignment.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
@@ -47,8 +48,39 @@ app.use((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end("<html><body><h1>This is an Express Server</h1></body></html>");
+=======
+const express = require('express'); 
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const campsiteRouter = require('./routes/campsiteRouter');
+const promotionRouter = require('./routes/promotionRouter');
+const partnerRouter = require('./routes/partnerRouter');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/campsites', campsiteRouter);
+app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
+
+app.use(express.static(__dirname + '/public'));  //get express to serve files from public folder
+
+app.use((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+>>>>>>> Stashed changes
 });
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
+<<<<<<< Updated upstream
 });
+=======
+});
+>>>>>>> Stashed changes
