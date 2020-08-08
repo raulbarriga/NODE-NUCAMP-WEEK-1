46fd7530e743ce489636ab3ac96e0898d9c7f024
Raul Barriga <barrigaraulaug@gmail.com>
2020-08-08 13:14:23 -0700

    Accepted changes from Melody's file

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,93 +1,59 @@
-<<<<<<< Updated upstream
 const express = require("express");
 const bodyParser = require("body-parser");
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
->>>>>>> Stashed changes
 
 const campsiteRouter = express.Router();
 
 campsiteRouter.use(bodyParser.json());
 
-<<<<<<< Updated upstream
 campsiteRouter
     .route("/")
-
-    //default for all routing methods for this path
     .all((req, res, next) => {
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/plain");
         next();
     })
-
-    //this is just to show that it's working
-    //won't use next() since we're not gonna use any more routing methods after this
     .get((req, res) => {
         res.end("Will send all the campsites to you");
     })
-
-    //from the app.all method, once it hits next(), it'll go to the next relevant method
-    //so it it was a post request, it'll skip the app.get request
     .post((req, res) => {
         res.end(
             `Will add the campsite: ${req.body.name} with description: ${req.body.description}`
         );
     })
-
-    //here we're rejesting the request with this statusCode & res.end()
     .put((req, res) => {
-        res.statusCode = 403;
+        res.statusCode = 403; //operation not supported
         res.end("PUT operation not supported on /campsites");
     })
-
-    //normally a dangerous operation, so you need to restrict it to only certain users!
     .delete((req, res) => {
         res.end("Deleting all campsites");
     });
 
-module.exports = campsiteRouter;
-=======
-campsiteRouter.route('/')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end('Will send all the campsites to you');
-})
-.post((req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.put((req, res) => {
-    res.statusCode = 403;  //operation not supported
-    res.end('PUT operation not supported on /campsites');
-})
-.delete((req, res) => {
-    res.end('Deleting all campsites');
-});
-
-campsiteRouter.route('/:campsiteId')
-.all((req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-.get((req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-})
-.post((req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-})
-.put((req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
-})
-.delete((req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+campsiteRouter
+    .route("/:campsiteId")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        next();
+    })
+    .get((req, res) => {
+        res.end(
+            `Will send details of the campsite: ${req.params.campsiteId} to you`
+        );
+    })
+    .post((req, res) => {
+        res.statusCode = 403;
+        res.end(
+            `POST operation not supported on /campsites/${req.params.campsiteId}`
+        );
+    })
+    .put((req, res) => {
+        res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+        res.end(
+            `Will update the campsite: ${req.body.name} with description: ${req.body.description}`
+        );
+    })
+    .delete((req, res) => {
+        res.end(`Deleting campsite: ${req.params.campsiteId}`);
+    });
 
 module.exports = campsiteRouter;
->>>>>>> Stashed changes
--- server.js
+++ server.js
@@ -1,86 +1,30 @@
-<<<<<<< Updated upstream
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+
 const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
 
 const hostname = "localhost";
 const port = 3000;
-const app = express();
 
+const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+
 app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
-//allows us to store whatever the client sends as part of the path after the '/'
-// as a route parameter named :campsiteId
-// app.get("/campsites/:campsiteId", (req, res) => {
-//     //here we're just echoing back the request to make sure we're receiving it correctly
-//     res.end(
-//         `Will send details of the campsite: ${req.params.campsiteId} to you`
-//     );
-// });
+app.use(express.static(__dirname + "/public")); //get express to serve files from public folder
 
-//we won't support a post request for campsites
-// app.post("/campsites/:campsiteId", (req, res) => {
-//     res.statusCode = 403;
-//     res.end(
-//         `POST operation not supported on /campsites/${req.params.campsiteId}`
-//     );
-// });
-
-// app.put("/campsites/:campsiteId", (req, res) => {
-//     // here we echo back from the JSON formatted body of the request message
-//     // instead of the reouter parameters
-//     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-//     res.end(`Will update the campsite: ${req.body.name}
-//         with description: ${req.body.description}`);
-// });
-
-// app.delete("/campsites/:campsiteId", (req, res) => {
-//     //endpoint for deleting a specific campsite
-//     res.end(`Deleting campsite: ${req.params.campsiteId}`);
-// });
-
-app.use(express.static(__dirname + "/public"));
 app.use((req, res) => {
-    // console.log(req.headers);
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     res.end("<html><body><h1>This is an Express Server</h1></body></html>");
-=======
-const express = require('express'); 
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const campsiteRouter = require('./routes/campsiteRouter');
-const promotionRouter = require('./routes/promotionRouter');
-const partnerRouter = require('./routes/partnerRouter');
-
-const hostname = 'localhost';
-const port = 3000;
-
-const app = express();
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-
-app.use('/campsites', campsiteRouter);
-app.use('/promotions', promotionRouter);
-app.use('/partners', partnerRouter);
-
-app.use(express.static(__dirname + '/public'));  //get express to serve files from public folder
-
-app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
->>>>>>> Stashed changes
 });
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-<<<<<<< Updated upstream
 });
-=======
-});
->>>>>>> Stashed changes
